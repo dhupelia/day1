6090f8945b4fa99f03d61598de829c14947b393a
Shekhar Dhupelia <shekhar.dhupelia@gmail.com>
2023-12-04 11:58:16 -0600

    Initial entry - day1 file

---
+++ day1.py
@@ -0,0 +1,21 @@
+#
+# Advent of Code 2023 - Day 1
+# Shekhar Dhupelia
+# 
+
+'''
+Plan
+====
+
+- Open input file
+- For each line
+- Read the line
+- If this was the last line / no more lines, print total and stop
+- Iterate through each character from the beginning until you find the first integer and save it as num1
+- Iterate through each character from the end until you find the first integer and save it as num2
+- Output num1,num2
+- Add num1 and num2 to total
+- End after end of file
+
+'''
+
