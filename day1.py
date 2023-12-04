db9196a4a3f84ddef84a4855485e70edcad70651
Shekhar Dhupelia <shekhar.dhupelia@gmail.com>
2023-12-04 12:35:10 -0600

    Saving WIP

--- day1.py
+++ day1.py
@@ -3,19 +3,41 @@
 # Shekhar Dhupelia
 # 
 
-'''
-Plan
-====
+# Open the file
+file = open("input.txt", "r")
 
-- Open input file
-- For each line
-- Read the line
-- If this was the last line / no more lines, print total and stop
-- Iterate through each character from the beginning until you find the first integer and save it as num1
-- Iterate through each character from the end until you find the first integer and save it as num2
-- Output num1,num2
-- Add num1 and num2 to total
-- End after end of file
+# Setup some counters for later
+total = 0
+num1 = 0
+num2 = 0
 
-'''
+# Loop through each line of text
+while True:
+    nextline = file.readline()
 
+    # Print the running total
+    print("Running Total = ", total, "\n")
+
+    # If there is no next line in the file, print the total result and break out of this loop
+    if not nextline:
+        print(total)
+        break
+
+    # Iterate through each character from the beginning until you find the first integer and save it as num1
+    while True:
+        for character in nextline:
+            print(character)
+            if character.isdigit():
+                num1 = character
+                print("Found a number!!!", "num1", "\n")
+                break
+            
+
+    # Iterate through each character from the end until you find the first integer and save it as num2
+
+    # Output both numbers for my sanity
+    print(num1)
+    print(num2)
+
+    # Add these two integers to our running total
+    total = total + num1 + num2
