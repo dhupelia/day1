5d38fa18de1b638654f93ca74f461ea9b41b7763
Shekhar Dhupelia <shekhar.dhupelia@gmail.com>
2023-12-04 14:37:47 -0600

    finished and tested

--- day1.py
+++ day1.py
@@ -8,41 +8,45 @@ file = open("input.txt", "r")
 
 # Setup some counters for later
 total = 0
-linecounter = 0
 num1 = -1
 num2 = -1
+combined = 0
+
+# Debug --- Setup a line counter
+linecounter = 0
 
 # Loop through each line of text
 for nextline in file:
 
-    # Increment the line counter for testing
-    linecounter = linecounter + 1
-
     # Prep the string for parsing
     line = nextline.strip('\n')
 
-    # Print the running total
-    print("Line", linecounter, "Running Total =", total)
+    # Reset num1 and num2
+    num1 = -1
+    num2 = -1
 
-    # Iterate through each character from the beginning until you find the first integer, add it to the total, and then ignore the rest
+    # Iterate through each character from the beginning until you find the first integer, and then ignore the rest
     for character in line:
-        if (num1 != -1):
+        if (num1 == -1):
             if character.isdigit():
                 num1 = int(character)
                 print("Found num1: ", num1)
-                total = total + num1
-        # Reset num1 now that we're done with the first integer
-        num1 = -1
 
-    # Iterate through each character from the end until you find the second integer, add it to the total, then add it to total
+    # Iterate through each character from the end until you find the second integer, and then ignore the rest
     for character in line [::-1]:
-        if (num2 != -1):
+        if (num2 == -1):
             if character.isdigit():
                 num2 = int(character)
                 print("Found num2: ", num2)
-                total = total + num2
-        # Reset num2 now that we're done with the first integer
-        num2 = -1
+
+    # Combine the two digits into a new two-digit number
+    combined = (num1 * 10) + num2
+    
+    # Add the combined number to the total
+    total = total + combined
+    
+    # Debug --- Increment the line counter for testing
+    linecounter = linecounter + 1
 
 # Print the final result
 print("\n\n**** The Final Result =", total, " ****\n\n")
